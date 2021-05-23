32e3acae5334414a26c1c28eec19abdd5e29206b
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-05-23 17:08:05 +0200

    The funcionality has been split between directories.

--- AlgebraSimulator/test.c
+++ AlgebraSimulator/test.c
@@ -1,7 +1,18 @@
 #include"parser.h"
 
 int main(void) {
-	char * buffer = "2 + 2 * 34";
-	printTokens(buffer);
+	printPromptMessage();
+
+	char *buffer = NULL;
+	int read;
+	size_t len;
+
+	read = getline(&buffer, &len, stdin);
+
+	if (-1 != read) {
+		printTokens(buffer);
+  } else {
+		printf("No line entered.");
+  }
 	return 0;
 }
